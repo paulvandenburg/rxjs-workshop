2feaf6d14a775e584c92a0ef4b84b3ad628c689c
Paul van den Burg <1727604+paulvandenburg@users.noreply.github.com>
2022-09-12 16:56:31 +0200

    Initial routing fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './workshop/base-layout/base-layout.component';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    children: [
+      {
+        path: 'workshop',
+        component: BaseLayoutComponent,
+        children: [
+          {
+            path: 'part-1',
+            loadChildren: () => import('./workshop/part1/part1.module').then((m) => m.Part1Module),
+          },
+          {
+            path: '',
+            pathMatch: 'full',
+            redirectTo: 'part-1',
+          },
+        ],
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'workshop',
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/workshop/part1/part1.module.ts
+++ src/app/workshop/part1/part1.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { P01tapComponent } from './p01tap/p01tap.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '01-tap',
+        component: P01tapComponent,
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '01-tap',
+      },
+    ],
+  },
+];
 
 @NgModule({
   declarations: [
@@ -9,6 +26,7 @@ import { P01tapComponent } from './p01tap/p01tap.component';
   ],
   imports: [
     CommonModule,
+    RouterModule.forChild(routes),
   ],
 })
 export class Part1Module {
