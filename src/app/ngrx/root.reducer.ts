9ae1d5ac6461095b992553a17f8f948fd64bad81
Paul van den Burg <1727604+paulvandenburg@users.noreply.github.com>
2023-10-26 14:59:05 +0200

    Fix eslint

--- src/app/ngrx/root.reducer.ts
+++ src/app/ngrx/root.reducer.ts
@@ -1,7 +1,6 @@
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface RootState {}
 
 export const reducers: ActionReducerMap<RootState> = {};
